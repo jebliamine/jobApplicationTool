23f6c355ced99e9acc7ede65f0e51d96e1639c67
Mohammed Amine Jebli <amine.jebli@alumni.fh-aachen.de>
2026-04-05 15:34:36 +0200

    first push: init project

---
+++ src/main/java/de/jeb/japp/JappApplication.java
@@ -0,0 +1,13 @@
+package de.jeb.japp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/jeb/japp/JappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jeb.japp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
